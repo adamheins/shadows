932a64b73344439fbb6bbcfa200d7fb64b6d3ab7
Adam Heins <mail@adamheins.com>
2024-10-24 12:20:17 -0400

    Clean up the code a bit.

--- scripts/main.py
+++ scripts/main.py
@@ -13,45 +13,31 @@ SCREEN_VERTS = np.array(
     [[0, 0], [SCREEN_WIDTH, 0], [SCREEN_WIDTH, SCREEN_HEIGHT], [0, SCREEN_HEIGHT]]
 )
 
-PHYSICS_STEP_PER_FRAME = 10
 FRAMERATE = 60
-TIMESTEP = 1.0 / (FRAMERATE * PHYSICS_STEP_PER_FRAME)
+PHYSICS_STEP_PER_TICK = 10
+TIMESTEP = 1.0 / (FRAMERATE * PHYSICS_STEP_PER_TICK)
 
-PLAYER_VELOCITY = 100  # px per second
+PLAYER_VELOCITY = 200  # px per second
 BULLET_VELOCITY = 1000
 
+PLAYER_MASS = 10
+BULLET_MASS = 0.1
 
-class Circle:
-    def __init__(self, position, radius, color):
-        self.position = position
-        self.radius = radius
-        self.color = color
-
-    def draw(self, surface):
-        pygame.draw.circle(surface, self.color, self.position, self.radius)
+# TODO shouldn't this be mapped to real time?
+SHOT_COOLDOWN_TICKS = 10
 
 
 def orth(v):
+    """Generate a 2D orthogonal to v."""
     return np.array([v[1], -v[0]])
 
 
 def unit(v):
+    """Normalize to a unit vector."""
     norm = np.linalg.norm(v)
-    if np.isclose(v, 0):
+    if np.isclose(norm, 0):
         return np.zeros_like(v)
     return v / norm
-
-
-# TODO need this to properly do the occlusions
-def line_line_intersection(p1, v1, p2, v2):
-    # parallel
-    if np.isclose(orth(v1) @ v2, 0):
-        return None
-
-    A = np.array([[-v1 @ v1, v1 @ v2], [-v1 @ v2, v2 @ v2]])
-    b = np.array([v1 @ (p1 - p2), v2 @ (p1 - p2)])
-    t = np.linalg.solve(A, b)
-    return p1 + t[0] * v1, ts
 
 
 def line_screen_edge_intersection(p, v):
@@ -103,7 +89,7 @@ class Obstacle:
                 break
         return right, left
 
-    def compute_occlusion(self, point, tol=1e-3):
+    def _compute_occlusion(self, point, tol=1e-3):
         right, left = self._compute_witness_vertices(point, tol=tol)
 
         delta_right = right - point
@@ -132,24 +118,24 @@ class Obstacle:
 
     def draw(self, surface, viewpoint=None):
         if viewpoint is not None:
-            ps = self.compute_occlusion(viewpoint)
+            ps = self._compute_occlusion(viewpoint)
             pygame.gfxdraw.aapolygon(surface, ps, (100, 100, 100))
             pygame.gfxdraw.filled_polygon(surface, ps, (100, 100, 100))
 
         pygame.draw.rect(surface, self.color, self.rect)
 
 
-class Player:
-    def __init__(self, space, position):
-        # self.body = pymunk.Body(body_type=pymunk.Body.KINEMATIC)
-        self.body = pymunk.Body(5, float("inf"))
-        self.body.position = position
+class Entity:
+    """Pymunk entity."""
 
-        self.shape = pymunk.Circle(self.body, 10, (0, 0))
-        self.shape.color = (255, 0, 0, 255)
-        self.shape.collision_type = 1
+    def __init__(self, space, body, shape):
+        self.body = body
+        self.shape = shape
+        space.add(body, shape)
 
-        space.add(self.body, self.shape)
+    @property
+    def id(self):
+        return self.body.id
 
     @property
     def position(self):
@@ -163,31 +149,74 @@ class Player:
     def velocity(self, value):
         self.body.velocity = tuple(value)
 
+    def remove(self):
+        space = self.body.space
+        space.remove(self.shape, self.body)
+
+
+class Agent(Entity):
+    def __init__(self, space, position, color):
+        body = pymunk.Body(PLAYER_MASS, float("inf"))
+        body.position = position
+
+        shape = pymunk.Circle(body, 10, (0, 0))
+        shape.color = color
+        shape.collision_type = 1
+
+        super().__init__(space, body, shape)
+
+        self.shot_cooldown = 0
+
     def draw(self, surface):
         # pygame.gfxdraw.aacircle(surface, int(self.position[0]), int(self.position[1]),
         #                         int(self.shape.radius), self.shape.color)
         pygame.draw.circle(surface, self.shape.color, self.position, self.shape.radius)
 
+    def tick(self):
+        self.shot_cooldown = max(0, self.shot_cooldown - 1)
 
-class Projectile:
-    def __init__(self, space, position, velocity):
-        self.body = pymunk.Body(0.1, float("inf"))
-        self.body.position = tuple(position)
-        self.body.velocity = tuple(velocity)
+    def shoot(self, target):
+        if self.shot_cooldown > 0:
+            return None
 
-        self.shape = pymunk.Circle(self.body, 3, (0, 0))
-        self.shape.color = (0, 0, 0, 255)
-        self.shape.collision_type = 2
+        norm = np.linalg.norm(target - self.position)
+        if norm > 0:
+            direction = (target - self.position) / norm
+            self.shot_cooldown = SHOT_COOLDOWN_TICKS
+            return Projectile(
+                space=self.body.space,
+                position=self.position,
+                velocity=BULLET_VELOCITY * direction,
+                agent_id=self.id,
+            )
+        else:
+            return None
 
-        space.add(self.body, self.shape)
+
+class Projectile(Entity):
+    def __init__(self, space, position, velocity, agent_id):
+        self.agent_id = agent_id
+
+        body = pymunk.Body(BULLET_MASS, float("inf"))
+        body.position = tuple(position)
+        body.velocity = tuple(velocity)
+
+        shape = pymunk.Circle(body, 3, (0, 0))
+        shape.color = (0, 0, 0, 255)
+        shape.collision_type = 2
+
+        super().__init__(space, body, shape)
 
     def draw(self, surface):
-        pygame.draw.circle(surface, self.shape.color, self.body.position, self.shape.radius)
+        pygame.draw.circle(
+            surface, self.shape.color, self.body.position, self.shape.radius
+        )
 
 
-class Controller:
-    def __init__(self):
-        pass
+class Blood:
+    def __init__(self, p1, p2):
+        self.p1 = p1
+        self.p2 = p2
 
 
 class Game:
@@ -197,48 +226,84 @@ class Game:
         self.screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
         self.clock = pygame.time.Clock()
         self.screen.fill((255, 255, 255))
+        self.screen_rect = self.screen.get_bounding_rect()
         pygame.display.flip()
 
         self.keys_down = set()
 
-        # TODO put in Controller
-        self.projectiles = []
+        self.projectiles = {}
 
-        # TODO
         self.obstacles = [
             Obstacle(self.space, 300, 300, 100, 100, color=(0, 0, 0)),
             Obstacle(self.space, 500, 300, 100, 20, color=(0, 0, 0)),
         ]
 
-        self.player = Player(space=self.space, position=[200, 200])
+        self.player = Agent(
+            space=self.space, position=[200, 200], color=(255, 0, 0, 255)
+        )
+        enemies = [Agent(space=self.space, position=[200, 300], color=(0, 0, 255, 255))]
+
+        self.agents = {enemy.body.id: enemy for enemy in enemies}
+        self.agents[self.player.body.id] = self.player
+
+        self._shot_cooldown = 0
+
+        self.bloods = []
 
         def bullet_obstacle_handler(arbiter, space, data):
-            # print(arbiter.shapes[1].body.velocity)
-            # v = arbiter.shapes[1].body.velocity
-            # n = arbiter.normal
-            # t = orth(n)
-            # # TODO apparently this does not work
-            # arbiter.shapes[1].body.velocity = tuple((v @ t) * t)
-            print("pow")
-            # TODO need to remove the projectile itself too...
-            space.remove(arbiter.shapes[1].body, arbiter.shapes[1])
+            # delete bullet when it hits an obstacle
+            body_id = arbiter.shapes[1].body.id
+            self.projectiles[body_id].remove()
+            self.projectiles.pop(body_id)
+            return False
+
+        def bullet_agent_handler(arbiter, space, data):
+            bullet_id = arbiter.shapes[1].body.id
+            agent_id = arbiter.shapes[0].body.id
+
+            bullet = self.projectiles[bullet_id]
+
+            # an agent cannot be hit by its own bullet
+            if bullet.agent_id == agent_id:
+                return False
+
+            if agent_id == self.player.body.id:
+                print("you lose!")
+            else:
+                agent = self.agents[agent_id]
+                agent.remove()
+                self.agents.pop(agent_id)
+
+                start = arbiter.contact_point_set.points[0].point_a
+                blood = Blood(start, start + 20 * unit(bullet.velocity))
+                self.bloods.append(blood)
+
             return False
 
         self.space.add_collision_handler(0, 2).begin = bullet_obstacle_handler
-        # self.draw_options = pymunk.pygame_util.DrawOptions(self.screen)
+        self.space.add_collision_handler(1, 2).begin = bullet_agent_handler
 
     def draw(self):
         self.screen.fill((255, 255, 255))
 
-        self.player.draw(self.screen)
+        for projectile in self.projectiles.values():
+            projectile.draw(self.screen)
 
         for obstacle in self.obstacles:
             obstacle.draw(self.screen, viewpoint=self.player.position)
 
-        for projectile in self.projectiles:
-            projectile.draw(self.screen)
+        for agent in self.agents.values():
+            agent.draw(self.screen)
+
+        for blood in self.bloods:
+            pygame.draw.line(self.screen, (255, 0, 0), blood.p1, blood.p2, width=3)
 
         pygame.display.flip()
+
+    def step(self):
+        # TODO we want to be able to step the sim forward once to facilitate
+        # learning
+        pass
 
     def loop(self):
         while True:
@@ -259,7 +324,11 @@ class Game:
 
             # respond to events
             velocity = np.zeros(2)
-            if pygame.K_d in self.keys_down:
+            # TODO add remaining checks like this and to the bullet as well
+            if (
+                pygame.K_d in self.keys_down
+                and self.player.position[0] < SCREEN_WIDTH - 10
+            ):
                 velocity[0] += 1
             if pygame.K_a in self.keys_down:
                 velocity[0] -= 1
@@ -272,42 +341,32 @@ class Game:
                 velocity = PLAYER_VELOCITY * velocity / norm
             self.player.velocity = tuple(velocity)
 
+            # create a new projectile
             if target is not None:
-                # TODO make a new projectile with appropriate direction and
-                # velocity
-                norm = np.linalg.norm(target - self.player.position)
-                if norm > 0:
-                    direction = (target - self.player.position) / norm
+                projectile = self.player.shoot(target)
+                if projectile is not None:
+                    self.projectiles[projectile.id] = projectile
 
-                    projectile = Projectile(
-                        space=self.space,
-                        position=self.player.position,
-                        velocity=BULLET_VELOCITY * direction,
-                    )
-                self.projectiles.append(projectile)
-            #
-            # for projectile in self.projectiles:
-            #     projectile.position += projectile.velocity
-            #     if (
-            #         projectile.position[0] < -projectile.radius
-            #         or projectile.position[1] < -projectile.radius
-            #         or projectile.position[0] > SCREEN_WIDTH + projectile.radius
-            #         or projectile.position[1] > SCREEN_HEIGHT + projectile.radius
-            #     ):
-            #         pass
-            #     # TODO delete if outside the screen
-            #
+            # remove projectiles that are outside of the screen
+            projectiles_to_remove = []
+            for idx, projectile in self.projectiles.items():
+                if not self.screen_rect.collidepoint(projectile.body.position):
+                    projectiles_to_remove.append(idx)
+            for idx in projectiles_to_remove:
+                self.projectiles[idx].remove()
+                self.projectiles.pop(idx)
+
             self.draw()
 
             # physics
-            for i in range(PHYSICS_STEP_PER_FRAME):
+            for _ in range(PHYSICS_STEP_PER_TICK):
                 self.space.step(TIMESTEP)
 
-            # graphics
-            self.clock.tick(FRAMERATE)
+            for agent in self.agents.values():
+                agent.tick()
 
-            # print(self.clock.get_fps())
-            # pygame.time.wait(DELAY)
+            # tick forward
+            self.clock.tick(FRAMERATE)
 
 
 def main():
